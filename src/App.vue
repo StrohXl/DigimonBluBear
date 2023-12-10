9429884aa4d04367b18ba57b8f111b704689ed1c
StrohXl <xaviermayora20@gmail.com>
2023-12-09 23:17:24 -0400

    return dist

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavHeader from './components/NavHeader.vue'
 </script>
 
 <template>
   <header class="shadow-2xl p-3 sm:p-3 md:p-4 bg-cyan-950 text-gray-50">
-    <nav class="flex justify-between items-center">
-      <h1 class="cursor-pointer font-bold font-serif text-md sm:text-sm md:text-base lg:text-lg">
-        <RouterLink to="/"> DigimonStore </RouterLink>
-      </h1>
-      <ul class="hidden md:flex">
-        <li class="cursor-pointer">
-          <RouterLink to="/"> Inicio </RouterLink>
-        </li>
-      </ul>
-    </nav>
+    <NavHeader />
   </header>
   <main>
     <RouterView />
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex justify-between items-center">
+    <h1 class="cursor-pointer font-bold font-serif text-sm sm:text-md md:text-base lg:text-lg">
+      <RouterLink to="/"> DigimonStore </RouterLink>
+    </h1>
+    <IconMenu class="h-5 cursor-pointer md:hidden" />
+    <ul class="hidden md:flex">
+      <li class="cursor-pointer">
+        <RouterLink to="/"> Inicio </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script setup lang="ts">
+import IconMenu from './icons/IconMenu.vue'
+</script>
